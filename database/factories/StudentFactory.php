3d77ca0478a342f0429155f956542aac3bdec3cb
Ouali Mohamed <mohamed.ouali2010gmail.com>
2024-03-20 12:19:55 +0100

    create student factory

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Student::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'ncin' => fake()->ean8(),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->phoneNumber(),
+            'address' => fake()->address(),
+        ];
+    }
+}
--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -14,20 +14,6 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        Student::create([
-            'ncin' => '12234567',
-            'name' => 'Sahli Amani',
-            'email' => 'sahli@amani@gmail.com',
-            'phone' => '57667668',
-            'address' => 'Adresse de Amani',
-        ]);
-
-        Student::create([
-            'ncin' => Str::random(8),
-            'name' => Str::random(10),
-            'email' => Str::random(10).'@gmail.com',
-            'phone' => Str::random(10),
-            'address' => Str::random(10),
-        ]);
+        Student::factory(5)->create();
     }
 }
